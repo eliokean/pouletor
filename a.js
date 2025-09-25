bd1c555aceedfa7e811f91305a7e8d9be0bc9b34
eliokean <eliottkenan@gmail.com>
2025-09-25 13:51:40 +0000

    c commit

---
+++ a.js
@@ -0,0 +1,15 @@
+{
+  "name": "Mon Application PWA",
+  "short_name": "MonApp",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
